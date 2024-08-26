8787804b34d46bb073fa3fb2b461e7a865d7327e
IldizovNodirbek <158564155+IldizovNodirbek@users.noreply.github.com>
2024-08-26 20:33:02 +0300

    Update Header.jsx

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,5 +1,4 @@
-import React, { useState } from 'react';  
-import { NavLink } from 'react-router-dom';  
+import React, { useState } from 'react';   
 import { RxHamburgerMenu } from "react-icons/rx";  
 import { RiDeleteBack2Fill } from "react-icons/ri";  
 
@@ -7,43 +6,43 @@ function Header() {
   const [isOpen, setIsOpen] = useState(false);  
 
   const toggleMenu = () => {  
-    setIsOpen(prevState => !prevState);  
+    setIsOpen(prevState => !prevState);   
   };  
 
   return (  
     <div className="container mx-auto">  
-      <header className='bg-white shadow-lg py-5 flex flex-col md:flex-row md:justify-between items-center px-5'>  
+      <header className='fixed top-0 left-0 right-0 bg-white shadow-lg py-5 flex flex-col md:flex-row md:justify-between items-center px-5 z-50 transform -translate-y-2'>  
         <h1 className='text-3xl md:text-4xl font-sans text-orange-700 mb-2 md:mb-0'>My Real Estate</h1>  
         
         <nav className={`md:flex md:items-center ${isOpen ? 'block' : 'hidden'} md:block`}>  
           <ul className='flex flex-col md:flex-row md:flex-nowrap space-y-2 md:space-y-0 md:space-x-6 text-[20px]'>  
-            {['home', 'about us', 'our features', 'on sale', 'our services', 'demo', 'our clients'].map((item) => (  
+            {['home', 'about', 'features', 'sale', 'demo', 'services', 'clients'].map(item => (  
               <li key={item}>  
-                <NavLink   
-                  to={item}   
+                <a   
+                  href={`#${item}`}  
                   className='relative text-gray-800 font-sans group'   
                   onClick={() => setIsOpen(false)}  
                 >  
-                  {item.split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ')}  
+                  {item.charAt(0).toUpperCase() + item.slice(1)}  
                   <span className='absolute left-0 right-0 bottom-0 h-[2px] bg-red-500 scale-x-0 transition-transform duration-300 group-hover:scale-x-100'></span>  
-                </NavLink>  
+                </a>  
               </li>  
             ))}  
           </ul>  
         </nav>  
         
-        <NavLink   
-          to="/contact"   
+        <a   
+          href="#contact"   
           className='py-[10px] px-[20px] bg-orange-700 border border-transparent rounded-lg text-lg text-white hover:bg-white transition-transform duration-500 hover:scale-125 hover:text-black hover:border-orange-700 mt-2 md:mt-0'   
           onClick={() => setIsOpen(false)}  
         >  
           Contact Us  
-        </NavLink>   
+        </a>   
         
         <button onClick={toggleMenu} className="md:hidden text-gray-800 mt-2 md:mt-0">  
           {isOpen ? <RiDeleteBack2Fill size={24} /> : <RxHamburgerMenu size={24} />}  
         </button>  
-      </header>  
+      </header>   
     </div>  
   );  
 }  
